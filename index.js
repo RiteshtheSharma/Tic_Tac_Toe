2a5b55f67ac500b87eb2d8e379fa67458adb1db6
RiteshtheSharma <72566311+RiteshtheSharma@users.noreply.github.com>
2024-01-01 16:49:25 +0530

    feat : gameBoard for inputting flags for user

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
-const gameBoardConstructor = (() => {
-  const gameboard = [[],[],[]];
-  return { gameboard };
-})();
-const PlayerConstructor = (name,flag) => {
-    
+const verifyFlag = (f) => flag + "" == "0" || flag.toLowerCase() === "x";
 
-  return {flag,}
-}
+const gameBoard = (() => {
+  const grid = [
+    [undefined, undefined, undefined],
+    [undefined, undefined, undefined],
+    [undefined, undefined, undefined],
+  ];
+  const getGrid = () => grid;
+  const updateGrid = (flag, rindex, cindex) => {
+    if (
+      verifyFlag(flag) &&
+      rindex >= 0 &&
+      rindex < 3 &&
+      cindex >= 0 &&
+      cindex < 3 &&
+      grid[rindex][cindex] === undefined
+    ) {
+    } else if (!verifyFlag(flag))
+      throw TypeError("Please check the value for flag parameter");
+    else if (rindex >= 0 && rindex < 3)
+      throw TypeError(
+        "Please check the value for rindex  parameter or other player has already put his/her flag at given grid position"
+      );
+    else if (cindex >= 0 && cindex < 3)
+      throw TypeError("Please check the value for cindex parameter");
+    else
+      throw TypeError(
+        "Other player has already put his/her flag at given grid position"
+      );
+  };
+  return { getGrid ,updateGrid };
+})();
+
+
+const PlayerConstructor = (name, flag) => {
+  if (verifyFlag(flag)) return { name, flag };
+  throw TypeError("flag can only be assigned '0' or 'x' ");
+};
